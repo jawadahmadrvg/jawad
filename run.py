152273a3a041457c80750988bfc0dd49e4dc61be
jawadahmadrvg <an5282270@gmail.com>
2025-06-25 04:46:07 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 import os,sys,platform
 bit = platform.architecture()[0]
 if bit == '64bit':
-    os.system('chmod +x rvg')
-    os.system('./rvg')
+    os.system('chmod +x ali')
+    os.system('./ali')
 elif bit == '32bit':
-    os.system('chmod +x rvg32')
-    os.system('./rvg32')
+    os.system('chmod +x ali32')
+    os.system('./ali32')
 else:
     print('device not supported')
